404dc37c08c0a5de7f01248daf17fa7c357c6863
gyuhason <rachels0705@naver.com>
2025-06-13 16:14:40 +0900

    code.h

--- Season-1/Level-2/code.h
+++ Season-1/Level-2/code.h
@@ -1,12 +1,3 @@
-// Welcome to Secure Code Game Season-1/Level-2!
-
-// Follow the instructions below to get started:
-
-// 1. Perform code review. Can you spot the bug? 
-// 2. Run tests.c to test the functionality
-// 3. Run hack.c and if passing then CONGRATS!
-// 4. Compare your solution with solution.c
-
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdio.h>
@@ -18,31 +9,16 @@
 #define MAX_USERS 100
 #define INVALID_USER_ID -1
 
-// For simplicity, both the private (implementation specific) and the public (API) parts 
-// of this application have been combined inside this header file. In the real-world, it
-// is expected for the public (API) parts only to be presented here. Therefore, for the 
-// purpose of this level, please assume that the private (implementation specific) sections 
-// of this file, would not be known to the non-privileged users of this application
-
-// Internal counter of user accounts
 int userid_next = 0;
-
-// The following structure is implementation-speicific and it's supposed to be unknown 
-// to non-privileged users
 typedef struct {
+    char username[MAX_USERNAME_LEN + 1];
     bool isAdmin;
     long userid;
-    char username[MAX_USERNAME_LEN + 1];
     long setting[SETTINGS_COUNT];
 } user_account;
 
-// Simulates an internal store of active user accounts
 user_account *accounts[MAX_USERS];
 
-// The signatures of the following four functions together with the previously introduced 
-// constants (see #DEFINEs) constitute the API of this module
-
-// Creates a new user account and returns it's unique identifier
 int create_user_account(bool isAdmin, const char *username) {
     if (userid_next >= MAX_USERS) {
         fprintf(stderr, "the maximum number of users have been exceeded");
@@ -67,8 +43,15 @@ int create_user_account(bool isAdmin, const char *username) {
     return userid_next++;
 }
 
-// Updates the matching setting for the specified user and returns the status of the operation
-// A setting is some arbitrary string associated with an index as a key
+bool update_username(int user_id, const char *new_name) {
+  if (user_id < 0 || user_id >= MAX_USERS)
+   return false;
+
+  int length = strlen(accounts[user_id]->username);
+  strncpy(accounts[user_id]->username+length, new_name, MAX_USERNAME_LEN);
+  return true;
+}
+
 bool update_setting(int user_id, const char *index, const char *value) {
     if (user_id < 0 || user_id >= MAX_USERS)
         return false;
@@ -80,13 +63,12 @@ bool update_setting(int user_id, const char *index, const char *value) {
         return false;
 
     v = strtol(value, &endptr, 10);
-    if (*endptr || i >= SETTINGS_COUNT)
+    if (*endptr || i < 0 || i >= SETTINGS_COUNT)
         return false;
     accounts[user_id]->setting[i] = v;
     return true;
 }
 
-// Returns whether the specified user is an admin
 bool is_admin(int user_id) {
     if (user_id < 0 || user_id >= MAX_USERS) {
         fprintf(stderr, "invalid user id");
@@ -95,9 +77,7 @@ bool is_admin(int user_id) {
     return accounts[user_id]->isAdmin;
 }
 
-// Returns the username of the specified user
 const char* username(int user_id) {
-    // Returns an error for invalid user ids
     if (user_id < 0 || user_id >= MAX_USERS) {
         fprintf(stderr, "invalid user id");
         return NULL;
