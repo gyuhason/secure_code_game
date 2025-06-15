22ffef04e3d82e0c5cc85db6e229fa6b153c5e30
gyuhason <rachels0705@naver.com>
2025-06-15 19:16:42 +0900

    Update test.js

--- Season-1/Level-1/test.js
+++ Season-1/Level-1/test.js
@@ -6,11 +6,11 @@ describe('Level-2 Testing', function() {
     it('Level-2 Testing succeed', function(done) {
         request(app)
             .get('/level-2')
-            .query({ number: '1e5' }) // 길이 3, 값 100000 → Success!
+            .query({ number: '1e5' }) // 길이 3, 값 100000 → Success!!
             .expect(200)
             .end(function(err, res) {
                 if (err) return done(err);
-                expect(res.text).to.equal('Success!');
+                expect(res.text).to.equal('Success!!');
                 done();
             });
     });
@@ -18,11 +18,11 @@ describe('Level-2 Testing', function() {
     it('Level-2 Testing failed', function(done) {
         request(app)
             .get('/level-2')
-            .query({ number: '123' }) // 길이 3, 값 123 → Failed!
+            .query({ number: '123' }) // 길이 3, 값 123 → Failed!!
             .expect(200)
             .end(function(err, res) {
                 if (err) return done(err);
-                expect(res.text).to.equal('Failed!');
+                expect(res.text).to.equal('Failed!!');
                 done();
             });
     });
